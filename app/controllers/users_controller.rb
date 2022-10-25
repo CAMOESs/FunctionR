a723498d95f48b8e5c0f2bbf3c7fb50b5c20dcb0
camoes <camoes@github.com>
2022-10-25 14:56:52 +0100

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    #devise_mailer.send(@user).deliver_later
-    ContactMailer.contact("sample@gmail.com").deliver_later
+    #devise_mailer.send("sample@gmail.com").deliver_later
+    ContactMailer.contact("sample@gmail.com").deliver_now
 
   end
 
